cc04fd16f22cba2875c385d69960d979e5caff6f
Umid Rza <umidrza4@gmail.com>
2026-04-27 15:40:06 +0400

    fix redis import

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
+const redis = require('../redis')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
